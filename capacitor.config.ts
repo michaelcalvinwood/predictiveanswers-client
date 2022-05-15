82eca0dd209f40e9006f86cab256a1ad57557479
Michael Wood <michaelwood33311@icloud.com>
2022-05-15 12:35:22 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'predictiveanswers-client',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
