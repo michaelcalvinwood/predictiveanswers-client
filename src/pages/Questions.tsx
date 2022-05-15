06d8b10dda5459b77c91cd5d7ffc5be355c5d963
Michael Wood <michaelwood33311@icloud.com>
2022-05-15 13:50:42 -0400

    added retrieval of questions via GET /questions

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,22 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+interface Questions {
+    num: number;
+    question: string;
+}
 
 const Questions: React.FC = () => {
+    const [curNum, setCurNum] = useState<number>(0);
+    const [questions, setQuestions] = useState<Questions[]>([]);
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>
-            Questions
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <h2>Questions</h2>
-      </IonContent>
-    </IonPage>
-  )
+    useEffect(() => {
+        
+        const request = {
+            url: `https://predictiveanswers.com:8080/questions`,
+            method: 'get',
+          }
+      
+          axios(request)
+          .then(res => {
+            console.log (res.data);
+            setQuestions(res.data);
+          })
+          .catch(err => {
+            console.log ('Questions axios get questions', err);
+          })
+    }, []);
+
+    return (
+        <IonPage className="questions">
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>
+                        Questions
+                    </IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCard className='questions__card'>
+                    <IonCardHeader className="ion-text-center">
+                        <IonCardTitle>
+                            Question {curNum+1}
+                        </IonCardTitle>
+                        <IonCardSubtitle>
+                    
+                        </IonCardSubtitle>
+                    </IonCardHeader>
+                    {questions && curNum < questions.length && <IonCardContent>
+                      <p className="questions__current-question">{questions[curNum].question}</p>
+                    </IonCardContent>}
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    )
 }
 
 export default Questions;
--- update-website.sh
+++ update-website.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+npm run build
 clear
 cd ./build
 rsync -a . root@predictiveanswers.com:/var/www/predictiveanswers.com/curBuild
