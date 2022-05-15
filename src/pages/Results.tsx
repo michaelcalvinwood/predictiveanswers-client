276ad006de9b515c8014b4632ce40502db365738
Michael Wood <michaelwood33311@icloud.com>
2022-05-15 19:15:36 -0400

    added retrieval of questioaire results via route GET /results and listing the results on the results page when the admin logs in

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,14 +1,58 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState, useEffect } from "react";
 import { logInOutline } from 'ionicons/icons';
+import axios from 'axios';
+
+interface AdminData {
+  id: string;
+  question_number: Number;
+  answer: string;
+}
+
 const Results: React.FC = () => {
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const [adminData, setAdminData] = useState<AdminData[]>([]);
+  const [lastRequestTs, setLastRequestTs] = useState<Number>(0);
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('token')) return;
+
+    // to avoid infinite loop, only allow data retrieval once per minute
+
+    const curTime = Date.now();
+    const diff = curTime - Number(lastRequestTs);
+    
+    console.log ('time diff', diff);
+
+    if ( diff < 60000) return;
+
+    setLastRequestTs(curTime);
+
+    const request = {
+      url: `https://predictiveanswers.com:8080/results`,
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('token')}`
+      }
+    }
+
+    axios(request)
+      .then(res => {
+        setIsAdmin(true);
+        setAdminData(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  });
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>
-            Results
+            {isAdmin ? 'Admin Results' : 'Results'}
           </IonTitle>
           <IonButtons slot="end">
             <IonButton routerLink="/login" color="light" routerDirection='none'>
@@ -18,7 +62,24 @@ const Results: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>Results</h2>
+        <IonRow>
+          <IonCol className="ion-align-self-center">Id</IonCol>
+          <IonCol className="ion-align-self-center">Number</IonCol>
+          <IonCol className="ion-align-self-center">Answer</IonCol>
+        </IonRow>
+        {!!adminData && 
+          <IonGrid>
+            {adminData.map(result => {
+              return (
+                <IonRow key={result.id + result.question_number}>
+                  <IonCol>{result.id}</IonCol>
+                  <IonCol>{result.question_number}</IonCol>
+                  <IonCol>{result.answer}</IonCol>
+                </IonRow>
+              )
+            })}
+          </IonGrid>
+        }
       </IonContent>
     </IonPage>
   )
