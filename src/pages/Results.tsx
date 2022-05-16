58512f95bb2b89e56bdc584684906b6b70e43b53
Michael Wood <michaelwood33311@icloud.com>
2022-05-15 21:37:51 -0400

    created builds for android and ios. Each build is in their respective directories.

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,3 +1,4 @@
+import './results.scss';
 import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
 import React, { useState, useEffect } from "react";
 import { logInOutline } from 'ionicons/icons';
@@ -47,6 +48,37 @@ const Results: React.FC = () => {
       })
   });
 
+  const generateCards = (answers: AdminData[]) => {
+    answers.sort((a: AdminData, b:AdminData) => {
+      if (a.id < b.id) return -1;
+      if (a.id > b.id) return 1;
+      if (a.question_number < b.question_number) return -1;
+      if (a.question_number > b.question_number) return 1;
+      return 0; 
+    })
+
+    let curId = '';
+    let output = '';
+
+    for (let i = 0; i < answers.length; ++i) {
+      if (answers[i].id !== curId) {
+        if (curId) {
+          output += '</IonCardContent></IonCard>' + "\n";
+        }
+        output += `<IonCard><IonCardHeader><IonCardTitle>${answers[i].id}</IonCardTItle></IonCardHeader></IonCardHeader>` + "\n";
+        output += '<IonCardContent>';
+        curId = answers[i].id;
+      }
+      output += `<p>${answers[i].question_number}: ${answers[i].answer}</p>`;
+    }
+    output += '</IonCardContent></IonCard>';
+
+    console.log (output);
+    return output;
+  }
+
+  if (adminData.length) generateCards(adminData);
+
   return (
     <IonPage>
       <IonHeader>
@@ -62,20 +94,23 @@ const Results: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonRow>
+        {/* <IonRow>
           <IonCol className="ion-align-self-center">Id</IonCol>
           <IonCol className="ion-align-self-center">Number</IonCol>
           <IonCol className="ion-align-self-center">Answer</IonCol>
-        </IonRow>
-        {!!adminData && 
+        </IonRow> */}
+        {!!adminData.length && 
           <IonGrid>
-            {adminData.map(result => {
+            {adminData.map((result, index) => {
               return (
-                <IonRow key={result.id + result.question_number}>
-                  <IonCol>{result.id}</IonCol>
-                  <IonCol>{result.question_number}</IonCol>
-                  <IonCol>{result.answer}</IonCol>
-                </IonRow>
+                <div key={result.id + result.question_number}>
+                  {index === 0 && <h3 className="results__title">{result.id}</h3>}
+                  {index > 0 && result.id !== adminData[index-1].id && <h3 className="results__title">{result.id}</h3>}
+                  <IonRow >
+                    <IonCol size="1">{result.question_number}</IonCol>
+                    <IonCol>{result.answer}</IonCol>
+                  </IonRow>
+                </div>
               )
             })}
           </IonGrid>
